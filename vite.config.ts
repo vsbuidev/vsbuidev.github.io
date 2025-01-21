0ab89a58b8e6793e26fcea4f38b2b807075c1c42
vsbuidev <vsb.uidev@gmail.com>
2025-01-21 12:17:47 +0530

    commit with action

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 // import pluginChecker from 'vite-plugin-checker';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    // pluginChecker({ typescript: true })
-  ]
+  plugins: [react()],
+  base: "/",
 });
